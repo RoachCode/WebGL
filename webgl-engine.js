a2494277b0607aaafa430e1589498211155daaa6
Bradley <74122730+RoachCode@users.noreply.github.com>
2023-04-27 15:37:54 -0700

    basic canvas setup

---
+++ webgl-engine.js
@@ -0,0 +1,18 @@
+main();
+
+function main()
+{
+    const canvas = document.querySelector('#glcanvas');
+    const gl = canvas.getContext('webgl');
+
+    if (gl === null)
+    {
+        alert('Unable to initialize WebGL.');
+        return;
+    }
+
+    // Set window clear colour to opaque black
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the colour buffer with the specified clear colour
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
